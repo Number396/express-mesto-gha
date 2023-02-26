b77636ed739604eebe26ad97fc953b4d737f1ca2
Number396 <number396@yandex.ru>
2023-02-26 11:54:46 +0300

    feat: add cards like & dislike routers

--- controllers/cards.js
+++ controllers/cards.js
@@ -19,3 +19,15 @@ module.exports.deletCardById = (req, res) => {
     .then((cards) => res.send({ data: cards }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
+
+module.exports.likeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+    .then((cards) => res.send({ data: cards }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
-const { createCard, getCards, deletCardById } = require('../controllers/cards');
+// eslint-disable-next-line object-curly-newline
+const { createCard, getCards, deletCardById, likeCard, dislikeCard } = require('../controllers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:cardId', deletCardById);
+router.put('/:cardId/likes', likeCard);
+router.delete('/:cardId/likes', dislikeCard);
 
 module.exports = router;
