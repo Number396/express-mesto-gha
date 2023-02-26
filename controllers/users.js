e08b7b32ee351f4cfebf97d57a13d99a3c4592da
Number396 <number396@yandex.ru>
2023-02-26 11:17:38 +0300

    feat: add avatar update router

--- controllers/users.js
+++ controllers/users.js
@@ -21,8 +21,14 @@ module.exports.getUserById = (req, res) => {
 
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
-  console.log(name, about, req.user._id);
   User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.updataAvatar = (req, res) => {
+  const { avatar } = req.body;
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
     .then((user) => res.send({ data: user }))
     .catch((err) => res.status(500).send({ message: err.message }));
 };
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 // eslint-disable-next-line object-curly-newline
-const { createUser, getUsers, getUserById, updateUser } = require('../controllers/users');
+const { createUser, getUsers, getUserById, updateUser, updataAvatar } = require('../controllers/users');
 
 router.get('/', getUsers);
 router.get('/:userId', getUserById);
 router.post('/', createUser);
 router.patch('/me', updateUser);
+router.patch('/me/avatar', updataAvatar);
 module.exports = router;
