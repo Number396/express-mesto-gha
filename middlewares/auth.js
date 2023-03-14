ea932bcbb1f1ae037f42dc18e16e9ce026d187fe
Number396 <number396@yandex.ru>
2023-03-14 21:53:03 +0300

    refactor: delete console.log in auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,6 @@ const UnauthorizedError = require('../errors/unauthorized-err');
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization || !authorization.startsWith('Bearer')) {
-    console.log('--inside auth');
     throw new UnauthorizedError('Необходима авторизация');
   }
   const jwt = authorization.replace('Bearer ', '');
@@ -12,7 +11,6 @@ module.exports.auth = (req, res, next) => {
   try {
     payload = jsonwebtoken.verify(jwt, 'secret_code');
   } catch (err) {
-    console.log('inside catch---');
     next(new UnauthorizedError('Необходима авторизация'));
   }
   req.user = payload;
